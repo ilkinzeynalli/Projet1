cc9d2c2abd593852f834a00e2723d8cbb1c1ca19
ilkin zeynalli <ilkinzeynalli@hotmail.com>
2020-04-04 10:01:29 -0700

    Add project files.

---
+++ StaticWebSiteMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StaticWebSiteMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("About")]
+        public ActionResult About()
+        {
+            return View();
+        }
+        [Route("Products")]
+        public ActionResult Products()
+        {
+            return View();
+        }
+        [Route("Store")]
+        public ActionResult Store()
+        {
+            return View();
+        }
+    }
+}
